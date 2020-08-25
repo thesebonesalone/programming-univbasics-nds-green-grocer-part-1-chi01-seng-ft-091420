c04459fd2e33fa1ba2a5cdf6a27c59f459bc376e
Willem A Duiker <duikhead2@gmail.com>
2020-08-25 16:31:54 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,10 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  
+  collection.each do |index|
+    puts index
+  end
 
 end
 
